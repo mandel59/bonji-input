9c9c91eed7fe0979bd14a7e1669090ee494f430d
Ryusei Yamaguchi <mandel59@gmail.com>
2021-05-02 12:05:42 +0900

    add Deno cli utility

---
+++ siddhamconv.js
@@ -0,0 +1,12 @@
+import { readLines } from "https://deno.land/std/io/mod.ts"
+import { ascii2siddham, ascii2symbol, latin2ascii } from "./siddham.js"
+
+const newline = new Uint8Array(['\n'.codePointAt(0)])
+const encoder = new TextEncoder()
+let firstLine = true
+for await (let line of readLines(Deno.stdin)) {
+    if (!firstLine) await Deno.stdout.write(newline)
+    const outline = ascii2siddham(ascii2symbol(latin2ascii(line)))
+    await Deno.stdout.write(encoder.encode(outline))
+    firstLine = false
+}
