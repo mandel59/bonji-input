80845481576756f107da8dad0ff0a5b0d1b159bf
Ryusei Yamaguchi <mandel59@gmail.com>
2021-11-25 01:27:08 +0900

    Enable deno lint

--- siddhamconv.js
+++ siddhamconv.js
@@ -4,7 +4,7 @@ import { ascii2siddham, ascii2symbol, latin2ascii } from "./siddham.js"
 const newline = new Uint8Array(['\n'.codePointAt(0)])
 const encoder = new TextEncoder()
 let firstLine = true
-for await (let line of readLines(Deno.stdin)) {
+for await (const line of readLines(Deno.stdin)) {
     if (!firstLine) await Deno.stdout.write(newline)
     const outline = ascii2siddham(ascii2symbol(latin2ascii(line)))
     await Deno.stdout.write(encoder.encode(outline))
